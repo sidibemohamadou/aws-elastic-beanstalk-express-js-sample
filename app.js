730e7ecfc3af315e7111a2439cbefd93428a94da
sidibemohamadou <mouhamadousidibe97@gmail.com>
2022-12-03 14:59:00 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Trainning AWS DEVOPS!'));
+app.get('/', (req, res) => res.send('Trainning AWS DEVOPS! FLK MS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
